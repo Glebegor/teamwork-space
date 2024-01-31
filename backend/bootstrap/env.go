7678ab09f3e9866bfa7fe702531f2b0190466470
Glebegor <glebegor2220@gmail.com>
2024-01-31 12:59:29 +0100

    BE: Setuped env,created first controllers.

--- backend/bootstrap/env.go
+++ backend/bootstrap/env.go
@@ -1,5 +1,12 @@
 package bootstrap
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
 type Env struct {
 	SERVERenv  string
 	SERVERport string
@@ -13,5 +20,23 @@ type Env struct {
 
 func NewEnv() (*Env, error) {
 	env := &Env{}
+	viper.SetConfigName("config")
+	viper.AddConfigPath("config")
+	if err := viper.ReadInConfig(); err != nil {
+		return env, err
+	}
+
+	if err := godotenv.Load(); err != nil {
+		return env, err
+	}
+	env.SERVERenv = viper.GetString("server.ENV")
+	env.SERVERport = viper.GetString("server.PORT")
+	env.DBport = viper.GetString("db.PORT")
+	env.DBhost = viper.GetString("db.HOST")
+	env.DBname = viper.GetString("db.NAME")
+	env.DBsslmode = viper.GetString("db.SSLMODE")
+	env.DBusername = viper.GetString("db.USERNAME")
+	env.DBpassword = os.Getenv("DB_PASSWORD")
+
 	return env, nil
 }
