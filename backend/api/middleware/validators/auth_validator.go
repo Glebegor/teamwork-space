46016db7f9a748cf87a909d4c86b005f2a9a60ca
Glebegor <glebegor2220@gmail.com>
2024-02-07 14:37:37 +0100

    BE: Started creation of validation for auth controller.

--- backend/api/middleware/validators/auth_validator.go
+++ backend/api/middleware/validators/auth_validator.go
@@ -1,1 +1,19 @@
 package validators
+
+import "github.com/gin-gonic/gin"
+
+func RegValidator() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+	}
+}
+func LoginValidator() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+	}
+}
+func RefreshValidator() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+	}
+}
