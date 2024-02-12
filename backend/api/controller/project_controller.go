094bddf1828385e22f4ea66947d12ba770d54428
Glebegor <glebegor2220@gmail.com>
2024-02-12 14:13:25 +0100

    BE: Started working with team api.

--- backend/api/controller/project_controller.go
+++ backend/api/controller/project_controller.go
@@ -1,1 +1,7 @@
 package controller
+
+import "team-work-space/domain"
+
+type ProjectController struct {
+	ProjectUsecase *domain.ProjectUsecase
+}
--- backend/domain/team.go
+++ backend/domain/team.go
@@ -1,1 +1,27 @@
 package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+const TeamCollection = "teams"
+
+type Team struct {
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	Subscribtion string             `json:"subscribtion" bson:"subscription"`
+	Owner        string             `json:"owner" bson:"owner"`
+	TeamMembers  string             `json:"teamMembers" bson:"teamMembers"`
+}
+type TeamUsecase interface {
+	Create()
+	GetAll()
+	GetById()
+	Delete()
+	Update()
+}
+type TeamRepository interface {
+	Create()
+	GetAll()
+	GetById()
+	Delete()
+	Update()
+}
