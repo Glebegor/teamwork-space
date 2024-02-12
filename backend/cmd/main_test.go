00f10e4bcdcf4cf0f9bf73fdd5e6c2ecf26bfd26
Glebegor <glebegor2220@gmail.com>
2024-02-12 16:23:51 +0100

    BE: Added main tests.

---
+++ backend/cmd/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"team-work-space/api/route"
+	"team-work-space/bootstrap"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	_ "team-work-space/docs"
+)
+
+func TestMain(m *testing.M) {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	app := bootstrap.App()
+	env := app.Env
+	db := app.Mongo.Database(env.DBname)
+
+	timeout := time.Duration(10) * time.Second
+
+	gin := gin.Default()
+	gin.Use(bootstrap.CORS())
+
+	route.SetupRoute(env, timeout, db, gin)
+	if env.SERVERenv == "development" {
+		fmt.Print("Running in development enviroment.\n	")
+	}
+	gin.Run(":" + env.SERVERport)
+
+}
