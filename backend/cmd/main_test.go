3edcb0b802fcc0e174e4f23b2d9a49f55817af46
Glebegor <glebegor2220@gmail.com>
2024-02-12 23:04:48 +0100

    BE: Added main_test, creating database test container.

--- backend/cmd/main_test.go
+++ backend/cmd/main_test.go
@@ -1,33 +1,60 @@
 package main
 
-//
-//import (
-//	"fmt"
-//	"github.com/sirupsen/logrus"
-//	"team-work-space/api/route"
-//	"team-work-space/bootstrap"
-//	"testing"
-//	"time"
-//
-//	"github.com/gin-gonic/gin"
-//
-//	_ "team-work-space/docs"
-//)
-//
-//func TestMain(m *testing.M) {
-//	logrus.SetFormatter(&logrus.JSONFormatter{})
-//
-//	app := bootstrap.App("test")
-//	env := app.Env
-//	db := app.Mongo.Database(env.DBname)
-//
-//	timeout := time.Duration(10) * time.Second
-//
-//	gin := gin.Default()
-//	gin.Use(bootstrap.CORS())
-//
-//	route.SetupRoute(env, timeout, db, gin)
-//	fmt.Print("Running in test enviroment.\n	")
-//	gin.Run(":" + env.SERVERport)
-//
-//}
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
+	"github.com/sirupsen/logrus"
+	"os"
+	"team-work-space/api/route"
+	"team-work-space/bootstrap"
+	_ "team-work-space/docs"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	_ = os.Chdir("..")
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	timeout := time.Duration(10) * time.Second
+
+	// Upping the mongo container
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		panic("Could not connect to Docker: " + err.Error())
+	}
+	options := &dockertest.RunOptions{
+		Repository: "mongo",
+		Tag:        "latest",
+		Name:       "db-team-work-space-test",
+		Env: []string{
+			"MONGO_INITDB_ROOT_USERNAME=admin",
+			"MONGO_INITDB_ROOT_PASSWORD=123321",
+		},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"27017/tcp": {
+				{HostIP: "", HostPort: "6464"},
+			},
+		},
+	}
+	_, err = pool.RunWithOptions(options)
+	if err != nil {
+		panic("Could not start resource: " + err.Error())
+	}
+	_, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	// Setup the app
+	app := bootstrap.App("test")
+	env := app.Env
+	db := app.Mongo.Database(env.DBname)
+
+	gin := gin.Default()
+	gin.Use(bootstrap.CORS())
+
+	route.SetupRoute(env, timeout, db, gin)
+	fmt.Print("Running in test enviroment.\n	")
+	//gin.Run(":" + env.SERVERport)
+}
